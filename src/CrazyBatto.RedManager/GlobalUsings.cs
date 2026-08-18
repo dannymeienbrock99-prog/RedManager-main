e407e2b3880a8e68e4af311f05a099c53e1f1d82
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-08-18 02:13:47 +0200

    Resolve WPF and Windows Forms type aliases

---
+++ src/CrazyBatto.RedManager/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Application = System.Windows.Application;
+global using Window = System.Windows.Window;
+global using MessageBox = System.Windows.MessageBox;
+global using MessageBoxButton = System.Windows.MessageBoxButton;
+global using MessageBoxImage = System.Windows.MessageBoxImage;
